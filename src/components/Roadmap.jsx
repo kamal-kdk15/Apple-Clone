e667fe2870596a6c365369cf8f2680fcc74b7c6e
Kamal Deep <kamalkdk15@gmail.com>
2024-09-03 17:23:18 +0530

    init

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,36 @@
+import { grid } from "../assets"
+import { roadmap } from "../constans"
+import Heading from "./Heading"
+import Section from "./Section"
+
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
+        <Heading
+        tag="Ready to get started"
+        title="What we’re working on"
+         />
+         <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
+          {roadmap.map((item) => {
+            const status = item.status === 'done' ? 'Done' : 'In progress';
+            return(
+              <div key={item.id}>
+                <div>
+                  <div>
+                    <img src={grid} />
+                    </div>
+                  </div>
+            
+                </div>
+            )
+            
+          })}
+         </div>
+      </div>
+    </Section>
+  )
+}
+
+export default Roadmap
